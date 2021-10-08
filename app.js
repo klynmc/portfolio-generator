2c04dd75b16f8a2e1ac33d4162ab986ad48eaddc
Leia <87383466+klynmc@users.noreply.github.com>
2021-10-08 15:06:06 -0500

    updated template

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -131,17 +133,13 @@ const promptProject = portfolioData => {
 }
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  console.log(portfolioData);
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+    console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-  // if (err) throw err;
-
-  // console.log('Portfolio complete! Check out index.html to see the output!');
-// });
